09330d7834177979b710905e13730ed6c8c6601d
huxx0229 <Shinny@x-10-250-229-144.uofm-guest.wireless.umn.edu>
2016-10-11 14:06:25 -0500

    Create the constructor of 'ThreeSum.java'

--- Algorithms/src/chapter_1/ThreeSum.java
+++ Algorithms/src/chapter_1/ThreeSum.java
@@ -2,7 +2,13 @@ package chapter_1;
 
 public class ThreeSum {
 	
+	public int N;
+	public int[] num;
 	
+	public ThreeSum (int N) {
+		num = new int[N];
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
