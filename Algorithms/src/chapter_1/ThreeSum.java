1ee2503d097691bb195a1de2dc7fbd725843fdd7
huxx0229 <Shinny@x-10-250-228-91.uofm-guest.wireless.umn.edu>
2016-10-11 19:04:35 -0500

    Testing...

--- Algorithms/src/chapter_1/ThreeSum.java
+++ Algorithms/src/chapter_1/ThreeSum.java
@@ -2,13 +2,7 @@ package chapter_1;
 
 public class ThreeSum {
 	
-	public int N;
-	public int[] num;
 	
-	public ThreeSum (int N) {
-		num = new int[N];
-		
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
