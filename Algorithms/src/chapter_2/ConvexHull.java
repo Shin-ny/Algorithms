9b608b85a0b988c9cfea0c55b6347d79e75a70bb
Shin_ny <ms.shinny.hoo@gmail.com>
2016-10-25 13:45:41 -0500

    Create ConvexHull.java and a bunch of information. Waiting to be implemented.

---
+++ Algorithms/src/chapter_2/ConvexHull.java
@@ -0,0 +1,37 @@
+package chapter_2;
+
+
+/*
+ * Document:
+ * "https://d18ky98rnyall9.cloudfront.net/_f03960834ff947e41e6b16e8431ab227_21ElementarySorts.pdf?Expires=1476835200&Signature=fjoGznRos0Wt0GF41F7KTEUxP~1mXT4UThikX4tycqptfVzJHLUnaXQFb1vUyAstew5jY2ZgeXNdvufFQfKTgSuIEbOEWibsjZQCFccw95VgqbDgIRsGA1El44aN4~QKJW~v35CZq1x-wP-7qBz0FxCE0y6y5E9m3Mmq5JR-z9U_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A"
+ * 
+ * Introduction: 
+ * The convex hull of a set of N points is 
+ * the smallest perimeter fence enclosing the points.
+ * 
+ * Output: 
+ * Sequence of vertices in counterclockwise order.
+ * 
+ * Implement Application:
+ * a) Mechanical algorithm:
+ * Hammer nails perpendicular to plane; stretch elastic rubber band around points.
+ * 
+ * b) Robot motion planning:
+ * Find shortest path in the plane from s to t that avoids a polygonal obstacle
+ * 
+ * c) Farthest pair problem:
+ * Given N points in the plane, find a pair of points 
+ * with the largest Euclidean distance between them.
+ * 
+ * More information:
+ * CCW: Given three points a, b, and c, is a → b → c a counterclockwise turn?
+ */
+public class ConvexHull {
+	
+	/*
+	 * As this implementation is better to be visually implemented
+	 * I'll leave it here for now
+	 * Got to finish it after getting canvas working
+	 */
+
+}
